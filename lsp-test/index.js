fa5c7e19e650c54efb08ea18beb000ae71f9c995
Gabrielito666 <galileo.fandango@gmail.com>
2026-03-27 13:32:53 -0300

    version: 1.0.0-beta

---
+++ lsp-test/index.js
@@ -0,0 +1,55 @@
+const Query = require("#lib/query/index.js");
+const DB = require("#lib/db/index.js");
+
+/**
+ * @import {IQuery} from "#lib/query/index.js"
+ */
+
+/**
+ * @class
+ * @implements {IQuery<"query-1", { name: string }, Error|void>}
+ * @extends {Query<"query-1", { name: string }, Error|void>}
+ */
+const Query1 = class extends Query
+{
+	constructor()
+	{
+		super("query-1", "SELECT");
+	}
+ 	/** @type {IQuery<"query-1", { name: string }, Error|void>["run"]}*/
+	run(pool, input)
+	{
+		return void 0;
+	}
+}
+
+
+/**
+ * @class
+ * @implements {IQuery<"query-2", { name2: string }, number>}
+ * @extends {Query<"query-2", { name2: string }, number>}
+ */
+const Query2 = class extends Query
+{
+	constructor()
+	{
+		super("query-2", "SELECT");
+	}
+ 	/** @type {IQuery<"query-2", { name2: string }, number>["run"]}*/
+	run(pool, input)
+	{
+		return 666;
+	}
+}
+
+const db = new DB({
+	db_host:"",
+	db_name:"",
+	db_port:1,
+	db_user:"",
+	db_password:""
+}, new Query1(), new Query2());
+
+db.exec("query-1", { name: "str" })
+db.exec("query-2", { name2: "" })
+
